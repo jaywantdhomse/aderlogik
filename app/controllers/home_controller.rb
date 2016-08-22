ac200e1cddb7decf43cb03c296129e50955c9d66
Jaywant <jaydhomse@gmail.com>
2016-08-22 12:40:22 +0530

    created pages and links

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,21 @@
 class HomeController < ApplicationController
+
   def index
   end
+  
+  def about_us
+  end
+  
+  def why_aderlogik
+  end
+  
+  def services
+  end
+  
+  def technology
+  end
+  
+  def contact_us
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  match "index" => "home#index", via: [:get, :post]
+  match "about_us" => "home#about_us", via: [:get, :post]
+  match "why_aderlogik" => "home#why_aderlogik", via: [:get, :post]
+  match "services" => "home#services", via: [:get, :post]
+  match "technology" => "home#technology", via: [:get, :post]
+  match "contact_us" => "home#contact_us", via: [:get, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
