f384d2f58e1a5c8264492893f0bbcce050a13d00
gaurav <gaurav.patil@zerebral.co.in>
2016-08-31 00:46:26 +0530

    changed defaults

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,10 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'jaydhomse@gmail.com'
+    default from: 'suvidhinathpadma@gmail.com'
 
     def contact_email(first_name, last_name, email)
         @first_name = first_name
         @last_name = last_name
         @email = email
-        mail(from: @email, subject: 'Contact Request')
+        mail(to: @email, subject: 'Contact Request')
     end
 end
