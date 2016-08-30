4ab53037cbf296aad3ff057f80079ea10f7564d7
Jaywant <jaydhomse@gmail.com>
2016-08-30 22:54:41 +0530

    chng

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,6 @@ module Aderlogik
     config.action_mailer.delivery_method = :sendmail
 	config.action_mailer.perform_deliveries = true
 	config.action_mailer.raise_delivery_errors = true
-	
+	config.assets.initialize_on_precompile = false
   end
 end
